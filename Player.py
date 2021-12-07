a7e6187fc4f06a4d4a81d77ff0ec92368e710c6f
arthurpaganini <arthurp2@illinois.edu>
2021-12-06 18:25:18 -0600

    defined Player class

---
+++ Player.py
@@ -0,0 +1,12 @@
+class Player:
+    def _init_(name, wonder, hand, board, might, resources, coins, player_left, player_right, player_across):
+        self.name = name
+        self.wonder = wonder
+        self.hand = hand
+        self.board = board
+        self.might = might
+        self.resources = resources
+        self.coins = coins
+        self.player_left = player_left
+        self.player_right = player_right
+        self.player_across = player_across
