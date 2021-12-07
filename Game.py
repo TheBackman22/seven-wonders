1f67b57556a90e9fab089d288e571a6e2f2e0a17
TheBackman22 <53570198+TheBackman22@users.noreply.github.com>
2021-12-06 18:58:08 -0600

    init

---
+++ Game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self, players, era = 1, round = 0):
+        self.players = players
+        self.era = era
+        self.round = round
+    
---
+++ Main.py
@@ -0,0 +1,4 @@
+from Game import Game
+players = ["p1", "p2"]
+game = Game(players)
+print(game.players[0])
